200a9d0390d789562c6709dfdbd5ed450c44a5fd
zengkaipeng <zkp0819@126.com>
2023-05-30 01:08:05 +0800

    sth wrong, batchlize failed, bug to be fix

--- inference_tools.py
+++ inference_tools.py
@@ -15,6 +15,7 @@ def greedy_inference_one(
         memory, mem_pad_mask = model.encode(graph)
         for idx in range(max_len):
             tgt_mask = generate_square_subsequent_mask(tgt.shape[1])
+            tgt_mask = tgt_mask.to(device)
             result = model.decode(
                 tgt=tgt, memory=memory, tgt_mask=tgt_mask,
                 memory_padding_mask=mem_pad_mask
@@ -29,3 +30,40 @@ def greedy_inference_one(
     answer = tokenizer.decode1d(tgt_list)
     answer = answer.replace(end_token, "").replace(begin_token, "")
     return answer
+
+
+def greedy_inference_batch(
+    model, tokenizer, graph, device, max_len,
+    begin_token='<CLS>', end_token='<END>'
+):
+    batch_size, model = graph.ptr.shape[0] - 1, model.eval()
+    tgt = torch.LongTensor([
+        tokenizer.encode1d([begin_token])
+    ] * batch_size).to(device)
+
+    end_id = tokenizer.token2idx[end_token]
+    alive = torch.ones(batch_size).bool()
+
+    with torch.no_grad():
+        memory, mem_pad_mask = model.encode(graph)
+        for idx in range(max_len):
+            tgt_mask = generate_square_subsequent_mask(tgt.shape[1])
+            tgt_mask = tgt_mask.to(device)
+            result = model.decode(
+                tgt=tgt[alive], memory=memory[alive], tgt_mask=tgt_mask,
+                memory_padding_mask=mem_pad_mask[alive]
+            )
+            result = result[:, -1].argmax(dim=-1)
+            padding_result = torch.ones(batch_size, 1) * end_id
+            padding_result = padding_result.long().to(device)
+            padding_result[alive] = result.unsqueeze(-1)
+            tgt = torch.cat([tgt, padding_result], dim=-1)
+            alive[alive][result == end_id] = False
+            if not torch.any(alive):
+                break
+    tgt_list = tgt.tolist()
+    answer = tokenizer.decode2d(tgt_list)
+    return [
+        x.replace(end_token, "").replace(begin_token, "")
+        for x in answer
+    ]
