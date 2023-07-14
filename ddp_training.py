c4974e0359ab96c125b614b8aeff1e917f65b589
zengkaipeng <zkp0819@126.com>
2023-07-14 17:36:28 +0800

    try to use ddp

---
+++ ddp_training.py
@@ -0,0 +1,50 @@
+import numpy as np
+import torch
+from data_utils import generate_tgt_mask
+from enum import Enum
+import torch.distributed as torch_dist
+
+
+class Summary(Enum):
+    NONE, SUM, AVERAGE, COUNT = 0, 1, 2, 3
+
+
+class MetricCollector(object):
+    def __init__(self, name, type_fmt=':f', summary_type=Summary.AVERAGE):
+        super(MetricCollector, self).__init__()
+        self.name, self.type_fmt = name, type_fmt
+        self.summary_type = summary_type
+
+    def reset(self):
+        self.val, self.sum, self.cnt, self.avg = [0] * 4
+
+    def update(self, val, num=1):
+        self.val = val
+        self.sum += val * num
+        self.cnt += num
+        self.avg = self.sum / self.cnt
+
+    def all_reduce(self):
+        infos = torch.FloatTensor([self.sum, self.cnt]).cuda()
+        torch_dist.all_reduce(infos, torch_dist.ReduceOp.SUM)
+        self.sum, self.cnt = infos.tolist()
+        self.avg = self.sum / self.cnt
+
+    def __str__(self):
+        return ''.join([
+            '{name}: {val', self.type_fmt, '} avg: {avg', self.type_fmt, '}'
+        ]).format(**self.__dict__)
+
+    def summary(self):
+        if self.summary_type is Summary.NONE:
+            fmtstr = ''
+        elif self.summary_type is Summary.AVERAGE:
+            fmtstr = '{name} {avg:.3f}'
+        elif self.summary_type is Summary.SUM:
+            fmtstr = '{name} {sum:.3f}'
+        elif self.summary_type is Summary.COUNT:
+            fmtstr = '{name} {cnt:.3f}'
+        else:
+            raise ValueError(f'Invaild summary type {self.summary_type} found')
+
+        return fmtstr.format(**self.__dict__)
