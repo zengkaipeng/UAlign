c7b679164489e52145aee4dbe07b5fdcfeb1d2ce
zengkaipeng <zkp0819@126.com>
2023-09-15 17:32:51 +0800

    data blocks almost done

--- data_utils.py
+++ data_utils.py
@@ -5,6 +5,7 @@ from utils.chemistry_parse import (
     BOND_FLOAT_TO_IDX, get_modified_atoms_bonds
 )
 from utils.graph_utils import smiles2graph
+from Dataset import OverallDataset
 from Dataset import BinaryEditDataset
 import random
 import torch
@@ -58,6 +59,52 @@ def create_edit_dataset(
         edges.append([(amap[i], amap[j]) for i, j in y])
 
     return BinaryEditDataset(graphs, nodes, edges, rxn_class=rxn_class)
+
+
+def extend_amap(amap, node_list):
+    result, max_node = {}, max(amap.values())
+    for node in node_list:
+        if node not in amap:
+            result[node] = max_node + 1
+            max_node += 1
+        else:
+            result[node] = amap[node]
+    return result
+
+
+def create_overall_dataset(
+    reacts, prods, rxn_class=None, kekulize=False,
+    verbose=True, pos_enc='none', **kwargs
+):
+    graphs, nodes, edges = [], [], []
+    node_types, edge_types = [], []
+    for idx, prod in enumerate(tqdm(prods) if verbose else prods):
+        # encoder_part
+        x, y = get_modified_atoms_bonds(reacts[idx], prod, kekulize)
+        encoder_graph, prod_amap = smiles2graph(
+            prod, with_amap=True, kekulize=kekulize
+        )
+        encoder_graph = add_pos_enc(encoder_graph, method=pos_enc, **kwargs)
+        graphs.append(graph)
+        nodes.append([amap[t] for t in x])
+        edges.append([(amap[i], amap[j]) for i, j in y])
+
+        node_type = get_node_types(reacts)
+        extend_amap = extend_amap(prod_amap, node_type.keys())
+        edge_type = get_edge_types(reacts, kekulize=kekulize)
+
+        real_n_types = {extend_amap[k]: v for k, v in node_type.items()}
+        real_e_types = {
+            (extend_amap[x], extend_amap[y]): v
+            for (x, y), v in edge_types.items()
+        }
+        node_types.append(real_n_types)
+        edge_types.append(real_e_types)
+        return OverallDataset(
+            graphs=graphs, activate_nodes=nodes, changed_edges=edges,
+            decoder_node_type=node_types, decoder_edge_type=edge_types,
+            rxn_class=rxn_class
+        )
 
 
 def load_data(data_dir, part):
