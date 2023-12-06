b7325f499f4b535d9c791274c85bf726186ffbbf
zengkaipeng <zkp0819@126.com>
2023-12-06 23:39:27 +0800

    bug fix

--- get_all_result.py
+++ get_all_result.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args_ft = eval(args.filter)
 
-    bpref, btime, bargs, bep, bap = None, None, None, None
-    bloss, btime2, bargs2, bep2, bap2 = None, None, None, None
+    bpref, btime, bargs, bep, bap = [None] * 5
+    bloss, btime2, bargs2, bep2, bap2 = [None] * 5
 
     for x in os.listdir(args.dir):
         if x.startswith('log-') and x.endswith('.json'):
@@ -37,14 +37,14 @@ if __name__ == '__main__':
             if len(INFO['valid_metric']) == 0:
                 continue
 
-            valid_losses = [x['all'] for x in INFO['valid_losses']]
+            valid_losses = [x['all'] for x in INFO['valid_loss']]
             this_ep = np.argmin(valid_losses)
 
-            if bloss is None or INFO['test_losses'][this_ep]['all'] < bloss:
-                bloss = INFO['test_losses'][this_ep]['all']
-                bep2, btime2, bargs2 = this_ep, timestamp, args
+            if bloss is None or INFO['test_loss'][this_ep]['all'] < bloss:
+                bloss = INFO['test_loss'][this_ep]['all']
+                bep2, btime2, bargs2 = this_ep, timestamp, INFO['args']
                 bap2 = {
-                    'loss': INFO['test_losses'][this_ep],
+                    'loss': INFO['test_loss'][this_ep],
                     'perf': INFO['test_metric'][this_ep]
                 }
 
@@ -53,17 +53,19 @@ if __name__ == '__main__':
 
             if bpref is None or INFO['test_metric'][this_ep]['all'] > bpref:
                 bpref = INFO['test_metric'][this_ep]['all']
-                bep, btime, bargs = this_ep, timestamp, args
+                bep, btime, bargs = this_ep, timestamp, INFO['args']
                 bap = {
-                    'loss': INFO['test_losses'][this_ep],
+                    'loss': INFO['test_loss'][this_ep],
                     'perf': INFO['test_metric'][this_ep]
                 }
 
+    print('[BY ACC]')
     print('[args]\n', json.dumps(bargs, indent=4))
     print('[TIME]', btime)
     print('[EPOCH]', bep)
     print('[pref]', json.dumps(bap, indent=4))
 
+    print('\n\n[BY LOSS]')
     print('[args]\n', json.dumps(bargs2, indent=4))
     print('[TIME]', btime2)
     print('[EPOCH]', bep2)
