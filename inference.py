b0ae3015ecc8d825d06c89549c989f69e2efe4fa
zengkaipeng <zkp0819@126.com>
2023-12-31 16:48:17 +0800

    inference

--- inference.py
+++ inference.py
@@ -1,121 +1,143 @@
-from rdkit import Chem
-import rdkit
-from torch.utils.data import DataLoader
 import torch
+import argparse
+import json
 import os
+import time
+import pickle
 
+
+from torch.utils.data import DataLoader
+from model import Graph2Seq, PositionalEncoding
+from training import train_trans, eval_trans
+from data_utils import fix_seed
+from torch.nn import TransformerDecoderLayer, TransformerDecoder
+from sparse_backBone import GINBase, GATBase
 from Mix_backbone import MixFormer
-from Dataset import edit_col_fn
-from model import BinaryGraphEditModel, convert_graphs_into_decoder
-from data_utils import create_edit_dataset
-from utils.chemistry_parse import convert_res_into_smiles
-
-
-def make_format_logits(node_logits, edge_index, useful_mask, edge_logits):
-    node_logits = node_logits.sigmoid()
-    edge_logits = edge_logits.sigmoid()
-    node_result = {}
-    for idx, prob in enumerate(node_logits):
-        node_result[idx] = prob.item()
-
-    edge_result = {}
-    used_edge = edge_index[:, useful_mask]
-    for idx, prob in enumerate(edge_logits):
-        src, dst = used_edge[:, idx]
-        src, dst = src.item(), dst.item()
-        if (src, dst) not in edge_result:
-            edge_result[(src, dst)] = edge_result[(dst, src)] = prob.item()
-        else:
-            t_res = (edge_result[(src, dst)] + prob.item()) / 2
-            edge_result[(src, dst)] = edge_result[(dst, src)] = t_res
-    return node_result, edge_result
+from utils.chemistry_parse import clear_map_number
 
 
 if __name__ == '__main__':
-
-    GNN_TYPE, GATE = 'gat', 'add'
-    DIM, HEAD, LAYER = 512, 8, 8
-    device = torch.device('cuda:0') if torch.cuda.is_available() \
-        else torch.device('cpu')
-
-    if GNN_TYPE == 'gin':
-        gnn_args = {'embedding_dim': DIM}
-    elif GNN_TYPE == 'gat':
-        assert DIM % HEAD == 0, \
-            'The model dim should be evenly divided by num_heads'
-        gnn_args = {
-            'in_channels': DIM, 'out_channels': DIM // HEAD,
-            'negative_slope': 0.2, 'dropout': 0.1,
-            'add_self_loop': False, 'edge_dim': DIM, 'heads': HEAD
-        }
-    else:
-        raise ValueError(f'Invalid GNN type {GNN_TYPE}')
-
-    GNN = MixFormer(
-        emb_dim=DIM, n_layers=LAYER, gnn_args=gnn_args,
-        dropout=0.1, heads=HEAD, pos_enc='none',
-        negative_slope=0.2, pos_args=None, n_class=None, edge_last=True,
-        residual=True, update_gate=GATE, gnn_type=GNN_TYPE
+    parser = argparse.ArgumentParser('Graph Edit Exp, Sparse Model')
+    parser.add_argument(
+        '--dim', default=256, type=int,
+        help='the hidden dim of model'
+    )
+    parser.add_argument(
+        '--n_layer', default=8, type=int,
+        help='the layer of encoder gnn'
+    )
+    parser.add_argument(
+        '--heads', default=4, type=int,
+        help='the number of heads for attention, only useful for gat'
+    )
+    parser.add_argument(
+        '--gnn_type', type=str, choices=['gat', 'gin'],
+        help='type of gnn backbone', required=True
+    )
+    parser.add_argument(
+        '--update_gate', choices=['add', 'cat'], type=str,
+        help='the update gate for graphtransformer'
+    )
+    parser.add_argument(
+        '--transformer', action='store_true',
+        help='use graph transformer or not'
+    )
+    parser.add_argument(
+        '--negative_slope', type=float, default=0.2,
+        help='negative slope for attention, only useful for gat'
+    )
+    parser.add_argument(
+        '--data_path', required=True, type=str,
+        help='the path containing dataset'
+    )
+    parser.add_argument(
+        '--seed', type=int, default=2023,
+        help='the seed for training'
+    )
+    parser.add_argument(
+        '--bs', type=int, default=512,
+        help='the batch size for training'
     )
 
-    model = BinaryGraphEditModel(GNN, DIM, DIM, 0.1).to(device)
+    parser.add_argument(
+        '--device', default=-1, type=int,
+        help='the device for running exps'
+    )
+    parser.add_argument(
+        '--checkpoint', type=str, required=True,
+        help='the path of checkpoint to restart the exp'
+    )
+    parser.add_argument(
+        '--token_ckpt', type=str, required=True,
+        help='the path of tokenizer, when ckpt is loaded, necessary'
+    )
 
-    rec = [
-        '[Br:1][c:2]1[cH:3][cH:4][c:5]([CH:6]=[O:7])[cH:11][cH:12]1.[CH2:8]([CH2:9][OH:10])[OH:13]',
-        '[CH2:9]([CH2:10][OH:11])[OH:12].[CH3:1][C:2]([c:3]1[cH:4][cH:5][cH:6][o:7]1)=[O:8]',
-        '[NH2:3][c:4]1[cH:5][cH:6][c:7]([N+:8](=[O:9])[O-:10])[cH:11][cH:12]1.[O:1]=[C:2]([C:13]([F:14])([F:15])[F:16])[O:19][C:18](=[O:17])[C:20]([F:21])([F:22])[F:23]',
-        '[CH3:1][CH:2]([CH3:3])[C:4](=[O:5])[Cl:10].[NH2:6][C:7]([NH2:8])=[S:9]'
-    ]
-    prod = [
-        '[Br:1][c:2]1[cH:3][cH:4][c:5]([CH:6]2[O:7][CH2:8][CH2:9][O:10]2)[cH:11][cH:12]1',
-        '[CH3:1][C:2]1([c:3]2[cH:4][cH:5][cH:6][o:7]2)[O:8][CH2:9][CH2:10][O:11]1',
-        '[O:1]=[C:2]([NH:3][c:4]1[cH:5][cH:6][c:7]([N+:8](=[O:9])[O-:10])[cH:11][cH:12]1)[C:13]([F:14])([F:15])[F:16]',
-        '[CH3:1][CH:2]([CH3:3])[C:4](=[O:5])[NH:6][C:7]([NH2:8])=[S:9]'
-    ]
+    args = parser.parse_args()
+    print(args)
 
-    dataset = create_edit_dataset(rec, prod, kekulize=False, rxn_class=None)
-    col_fn = edit_col_fn(selfloop=GNN_TYPE == 'gat')
-    loader = DataLoader(dataset, collate_fn=col_fn, batch_size=2)
+    if not torch.cuda.is_available() or args.device < 0:
+        device = torch.device('cpu')
+    else:
+        device = torch.device(f'cuda:{args.device}')
 
-    weight_dir = f'../retro_iclr_result/log_edit/wo_class/Gtrans_{GNN_TYPE}'
-    weight_name = 'fit-1694174494.1151376.pth'
+    fix_seed(args.seed)
+    with open(args.token_ckpt, 'rb') as Fin:
+        tokenizer = pickle.load(Fin)
 
-    model_path = os.path.join(weight_dir, weight_name)
+    if args.transformer:
+        if args.gnn_type == 'gin':
+            gnn_args = {
+                'in_channels': args.dim, 'out_channels': args.dim,
+                'edge_dim': args.dim
+            }
+        elif args.gnn_type == 'gat':
+            assert args.dim % args.heads == 0, \
+                'The model dim should be evenly divided by num_heads'
+            gnn_args = {
+                'in_channels': args.dim, 'dropout': args.dropout,
+                'out_channels': args.dim // args.heads, 'edge_dim': args.dim,
+                'negative_slope': args.negative_slope, 'heads': args.heads
+            }
+        else:
+            raise ValueError(f'Invalid GNN type {args.backbone}')
 
-    weight = torch.load(model_path, map_location=device)
-
-    model.load_state_dict(weight)
-
-    for data in loader:
-        data = data.to(device)
-        with torch.no_grad():
-            nod_log, useful_mask, edge_log = model.predict_logitis(data)
-            node_res, edge_res = make_format_logits(
-                nod_log, data.edge_index, useful_mask, edge_log
+        GNN = MixFormer(
+            emb_dim=args.dim, n_layers=args.n_layer, gnn_args=gnn_args,
+            dropout=args.dropout, heads=args.heads, gnn_type=args.gnn_type,
+            n_class=None, update_gate=args.update_gate
+        )
+    else:
+        if args.gnn_type == 'gin':
+            GNN = GINBase(
+                num_layers=args.n_layer, dropout=args.dropout,
+                embedding_dim=args.dim, n_class=None
             )
-            out_graphs = model.predict_into_graphs(data)
+        elif args.gnn_type == 'gat':
+            GNN = GATBase(
+                num_layers=args.n_layer, dropout=args.dropout,
+                embedding_dim=args.dim, num_heads=args.heads,
+                negative_slope=args.negative_slope, n_class=None
+            )
+        else:
+            raise ValueError(f'Invalid GNN type {args.backbone}')
 
-            print(node_res, edge_res)
-            print(out_graphs)
+    decode_layer = TransformerDecoderLayer(
+        d_model=args.dim, nhead=args.heads, batch_first=True,
+        dim_feedforward=args.dim * 2, dropout=args.dropout
+    )
+    Decoder = TransformerDecoder(decode_layer, args.n_layer)
+    Pos_env = PositionalEncoding(args.dim, args.dropout, maxlen=2000)
 
-            result = convert_graphs_into_decoder(out_graphs, 10)
-            print(result)
+    model = Graph2Seq(
+        token_size=tokenizer.get_token_size(), encoder=GNN,
+        decoder=Decoder, d_model=args.dim, pos_enc=Pos_env
+    ).to(device)
 
-    print('[Making moles]')
+    if args.checkpoint != '':
+        assert args.token_ckpt != '', 'Missing Tokenizer Information'
+        print(f'[INFO] Loading model weight in {args.checkpoint}')
+        weight = torch.load(args.checkpoint, map_location=device)
+        model.load_state_dict(weight)
 
-    org_node_type = {
-        0: 7, 1: 15, 2: 15, 3: 15, 4: 15, 5: 17, 6: 17,
-        7: 17, 8: 17, 9: 2, 10: 15, 11: 15
-    }
-    org_edge_type = {
-        (0, 1): 1, (1, 2): 2, (2, 3): 1, (3, 4): 2, (4, 5): 1,
-        (7, 8): 1, (8, 9): 1, (4, 10): 1, (10, 11): 2, (11, 1): 1
-    }
-
-    pred_node_type = {12: 35}
-    pred_edge_type = {(7, 12): 1, (5, 6): 2}
-
-    print(convert_res_into_smiles(
-        org_node_type, org_edge_type,
-        pred_node_type, pred_edge_type
-    ))
+    print('[INFO] padding index', tokenizer.token2idx['<PAD>'])
+    
