af4f48b07944de223be3f20015eeafea7d65f69a
zengkaipeng <zkp0819@126.com>
2024-01-05 12:50:15 +0800

    remove PE

--- model.py
+++ model.py
@@ -397,3 +397,83 @@ class PretrainModel(torch.nn.Module):
         )
 
         return result
+
+
+class NoPE(torch.nn.Module):
+    def __init__(self, token_size, encoder, decoder, d_model, pos_enc):
+        super(NoPE, self).__init__()
+        self.word_emb = torch.nn.Embedding(token_size, d_model)
+        self.encoder, self.decoder = encoder, decoder
+        self.Hchange = torch.nn.Sequential(
+            torch.nn.Linear(d_model, d_model),
+            torch.nn.ReLU(),
+            torch.nn.Linear(d_model, 2)
+        )
+
+        self.Cchange = torch.nn.Sequential(
+            torch.nn.Linear(d_model, d_model),
+            torch.nn.ReLU(),
+            torch.nn.Linear(d_model, 2)
+        )
+        self.Echange = torch.nn.Sequential(
+            torch.nn.Linear(d_model, d_model),
+            torch.nn.ReLU(),
+            torch.nn.Linear(d_model, 2)
+        )
+
+        self.edge_cls = torch.nn.Sequential(
+            torch.nn.Linear(d_model, d_model),
+            torch.nn.ReLU(),
+            torch.nn.Linear(d_model, 5)
+        )
+        self.pos_enc = pos_enc
+        self.output_layer = torch.nn.Sequential(
+            torch.nn.Linear(d_model, d_model),
+            torch.nn.ReLU(),
+            torch.nn.Linear(d_model, token_size)
+        )
+
+    def graph2batch(
+        self, node_feat: torch.Tensor, batch_mask: torch.Tensor,
+    ) -> torch.Tensor:
+        batch_size, max_node = batch_mask.shape
+        answer = torch.zeros(batch_size, max_node, node_feat.shape[-1])
+        answer = answer.to(node_feat.device)
+        answer[batch_mask] = node_feat
+        return answer
+
+    def encode(self, graphs):
+        node_feat, edge_feat = self.encoder(graphs)
+        memory = self.graph2batch(node_feat, graphs.batch_mask)
+        return memory, torch.logical_not(graphs.batch_mask)
+
+    def decode(
+        self, tgt, memory, memory_padding_mask=None,
+        tgt_mask=None, tgt_padding_mask=None
+    ):
+        tgt_emb = self.pos_enc(self.word_emb(tgt))
+        result = self.decoder(
+            tgt=tgt_emb, memory=memory, tgt_mask=tgt_mask,
+            memory_key_padding_mask=memory_padding_mask,
+            tgt_key_padding_mask=tgt_padding_mask
+        )
+        return self.output_layer(result)
+
+    def forward(self, graphs, tgt, tgt_mask, tgt_pad_mask):
+        tgt_emb = self.pos_enc(self.word_emb(tgt))
+        node_feat, edge_feat = self.encoder(graphs)
+
+        memory = self.graph2batch(node_feat, graphs.batch_mask)
+        result = self.output_layer(self.decoder(
+            tgt=tgt_emb, memory=memory, tgt_mask=tgt_mask,
+            memory_key_padding_mask=torch.logical_not(graphs.batch_mask),
+            tgt_key_padding_mask=tgt_pad_mask
+        ))
+
+        AH_logs = self.Hchange(node_feat)
+        AE_logs = self.Echange(node_feat)
+        AC_logs = self.Cchange(node_feat)
+
+        edge_logs = self.edge_cls(edge_feat)
+
+        return edge_logs, AH_logs, AE_logs, AC_logs, result
