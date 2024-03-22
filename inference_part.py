afa8a02ea70849aaebfe635e7d50124e6e314f29
zengkaipeng <zkp0819@126.com>
2024-03-23 01:42:36 +0800

    xd

--- inference_part.py
+++ inference_part.py
@@ -2,26 +2,20 @@ from tqdm import tqdm
 import torch
 import argparse
 import json
-import os
-import time
 import pickle
 
 
 from torch.utils.data import DataLoader
-from model import Graph2Seq, PositionalEncoding
-from training import train_trans, eval_trans
+from model import PretrainModel, PositionalEncoding
 from data_utils import fix_seed
 from torch.nn import TransformerDecoderLayer, TransformerDecoder
-from sparse_backBone import GINBase, GATBase
-from Mix_backbone import MixFormer
-from utils.chemistry_parse import clear_map_number, cano_with_am
+from sparse_backBone import GATBase
+from utils.chemistry_parse import clear_map_number
 from utils.graph_utils import smiles2graph
 import pandas
 import torch_geometric
 from inference_tools import beam_search_one
 import time
-import numpy as np
-from rdkit import Chem
 import os
 
 
@@ -49,7 +43,7 @@ def make_graph_batch(smi, rxn=None):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Graph Edit Exp, Sparse Model')
+    parser = argparse.ArgumentParser('Inference part')
     parser.add_argument(
         '--dim', default=256, type=int,
         help='the hidden dim of model'
@@ -61,18 +55,6 @@ if __name__ == '__main__':
     parser.add_argument(
         '--heads', default=4, type=int,
         help='the number of heads for attention, only useful for gat'
-    )
-    parser.add_argument(
-        '--gnn_type', type=str, choices=['gat', 'gin'],
-        help='type of gnn backbone', required=True
-    )
-    parser.add_argument(
-        '--update_gate', choices=['add', 'cat'], type=str,
-        help='the update gate for graphtransformer'
-    )
-    parser.add_argument(
-        '--transformer', action='store_true',
-        help='use graph transformer or not'
     )
     parser.add_argument(
         '--negative_slope', type=float, default=0.2,
@@ -135,44 +117,11 @@ if __name__ == '__main__':
     with open(args.token_ckpt, 'rb') as Fin:
         tokenizer = pickle.load(Fin)
 
-    if args.transformer:
-        if args.gnn_type == 'gin':
-            gnn_args = {
-                'in_channels': args.dim, 'out_channels': args.dim,
-                'edge_dim': args.dim
-            }
-        elif args.gnn_type == 'gat':
-            assert args.dim % args.heads == 0, \
-                'The model dim should be evenly divided by num_heads'
-            gnn_args = {
-                'in_channels': args.dim, 'dropout': 0.1,
-                'out_channels': args.dim // args.heads, 'edge_dim': args.dim,
-                'negative_slope': args.negative_slope, 'heads': args.heads
-            }
-        else:
-            raise ValueError(f'Invalid GNN type {args.backbone}')
-
-        GNN = MixFormer(
-            emb_dim=args.dim, n_layers=args.n_layer, gnn_args=gnn_args,
-            dropout=0.1, heads=args.heads, gnn_type=args.gnn_type,
-            n_class=11 if args.use_class else None,
-            update_gate=args.update_gate
-        )
-    else:
-        if args.gnn_type == 'gin':
-            GNN = GINBase(
-                num_layers=args.n_layer, dropout=0.1,
-                embedding_dim=args.dim, n_class=11 if args.use_class else None
-            )
-        elif args.gnn_type == 'gat':
-            GNN = GATBase(
-                num_layers=args.n_layer, dropout=0.1,
-                embedding_dim=args.dim, num_heads=args.heads,
-                negative_slope=args.negative_slope,
-                n_class=11 if args.use_class else None
-            )
-        else:
-            raise ValueError(f'Invalid GNN type {args.backbone}')
+    GNN = GATBase(
+        num_layers=args.n_layer, dropout=0.1, embedding_dim=args.dim,
+        num_heads=args.heads, negative_slope=args.negative_slope,
+        n_class=11 if args.use_class else None
+    )
 
     decode_layer = TransformerDecoderLayer(
         d_model=args.dim, nhead=args.heads, batch_first=True,
@@ -181,7 +130,7 @@ if __name__ == '__main__':
     Decoder = TransformerDecoder(decode_layer, args.n_layer)
     Pos_env = PositionalEncoding(args.dim, 0.1, maxlen=2000)
 
-    model = Graph2Seq(
+    model = PretrainModel(
         token_size=tokenizer.get_token_size(), encoder=GNN,
         decoder=Decoder, d_model=args.dim, pos_enc=Pos_env
     ).to(device)
@@ -190,7 +139,7 @@ if __name__ == '__main__':
         assert args.token_ckpt != '', 'Missing Tokenizer Information'
         print(f'[INFO] Loading model weight in {args.checkpoint}')
         weight = torch.load(args.checkpoint, map_location=device)
-        model.load_state_dict(weight, strict=False)
+        model.load_state_dict(weight, strict=True)
 
     print('[INFO] padding index', tokenizer.token2idx['<PAD>'])
 
@@ -212,7 +161,7 @@ if __name__ == '__main__':
 
     for idx, resu in enumerate(tqdm(meta_df['reactants>reagents>production'])):
         rea, prd = resu.strip().split('>>')
-        prd = cano_with_am(prd)
+        prd = clear_map_number(prd)
         rea = clear_map_number(rea)
         if args.use_class:
             rxn_class = int(meta_df['class'][idx])
